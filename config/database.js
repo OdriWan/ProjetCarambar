f123ce370fcfefcde0630eb341f9c550e1cf273d
Audrey <74775317+OdriWan@users.noreply.github.com>
2024-06-23 14:44:51 +0200

    Déploiement github pages

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-//Configuration de la base de données
-const sequelize = new Sequelize("test-db", "user", "pass", {
-  dialect: "sqlite",
-  host: "./dev.sqlite",
-});
-
-module.exports = sequelize;
